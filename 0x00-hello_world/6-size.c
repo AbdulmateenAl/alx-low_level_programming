0ddde7fc0299c516846c98f824ae2669d23ab8d6
Abdulmateen <alabiabdulmateen94@gmail.com>
2023-03-18 19:39:29 -0700

    assembles

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,4 @@
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)", sizeof(char));
-	printf("size of an init: %d byte(s)", sizeof(int));
-	printf("size of long int: %d byte(s)", sizeof(longint));
-	printf("size of a long long int: %d byte(s)", sizeof(long long int));
-	printf("size of float: %d byte(s", sizeof(float));
-	return (0);
+	printf("sixe of a char: % byte(s)",
