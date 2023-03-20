3c8bc2ef15d84efd94a76ad29506c1d5a78afd5c
Abdulmateen <alabiabdulmateen94@gmail.com>
2023-03-20 07:01:04 -0700

    print exactly programming is like...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  *main - main entry point
- *return - always o (success)
+ *My_function - prints out programming is like...
+ *Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle,");
 	return (0);
 }
