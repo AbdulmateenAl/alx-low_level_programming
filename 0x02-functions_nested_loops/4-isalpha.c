44c1ba55ac16bc872772c7d4d0ac084e97b9a1d8
Abdulmateen <alabiabdulmateen94@gmail.com>
2023-03-21 16:08:56 -0700

    updated

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,6 +3,7 @@
 /**
  * _isalpha - detect lower or uppercase letters
  * Return: 1 for a letter either lowercase or uppercase or 0 for otherwise
+ * @c: The chracter to be detected
  */
 
 int _isalpha(int c)
